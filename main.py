bf942ef84286037aeb2001a67f1af121f686bcfd
Vishesh <visheshpagare21@gmail.com>
2026-05-27 13:48:10 +0530

    syntra

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from google import genai
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = genai.Client(
+    api_key="AIzaSyD7uiVrydpPQ95xQMI2aw0lWLDMMF22g-8"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message":"Syntra AI Running"
+    }
+
+@app.get("/chat")
+def chat(q:str):
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=q
+    )
+
+    return {
+        "answer":response.text
+    }
